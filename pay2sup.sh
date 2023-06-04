7cdd0346fb72bf62e2c32b32da3bd8f182e29fea
elfametesar <erenmetesar@gmail.com>
2023-06-04 20:10:54 +0300

    Minor mistake corrected

--- pay2sup.sh
+++ pay2sup.sh
@@ -399,7 +399,7 @@ recovery() {
 	read_write 2>> $LOG_FILE
 	recovery_resize 2>> $LOG_FILE
 	pack 2>> $LOG_FILE
-	if [[ -z $RECOVERY ]]; then
+	if [[ $RECOVERY == 1 ]]; then
 		echo "Moving super image to $OUT, you can flash it in recovery from there"
 		mv $HOME/flashable/super.img $OUT
 	else
